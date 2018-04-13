760e096161d01c54b5a7b2ce430dd078acdd9c18
Harshad Ambulkar <30590@Hexaware.com>
2018-04-13 22:20:00 +0530

    3 way initialize done

--- MyProject/src/date_2016_04_13/InitializingObject_3_Ways.java
+++ MyProject/src/date_2016_04_13/InitializingObject_3_Ways.java
@@ -1,10 +1,24 @@
 package date_2016_04_13;
 
+class employee{
+	int emp_id=100;
+	public employee() {
+		System.out.println("Default Constructor called, value set to : "+emp_id);
+	}
+	public employee(int i) {
+	emp_id = i;
+	System.out.println("Constructor called, value set to : "+emp_id);
+	}
+	public void set_emp_id(int i) {
+		emp_id = i;
+	}
+}
+
 public class InitializingObject_3_Ways {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		employee obj_1 = new employee();
+		//employee obj_2 = new employee();		
 	}
-
 }
